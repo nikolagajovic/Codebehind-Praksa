593842cc56120f38935ab507b6c68c28a2d846d8
nikolagajovic <iamnikolagajovic@gmail.com>
2024-09-02 14:08:35 +0200

    fix bag simulate game

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ function simulateGame(team1, team2, allowDraw = false) {
     const probabilityTeam1Win = 1 / (1 + Math.pow(10, rankDifference / 400));
     const randomValue = Math.random();
     const team1Wins = randomValue < probabilityTeam1Win;
-
-    let team1Score = Math.floor(Math.random() * 20) + 80; // Use let instead of const
-    let team2Score = Math.floor(Math.random() * 20) + 80; // Use let instead of const
+    const team1Score = Math.floor(Math.random() * 20) + 80;
+    const team2Score = Math.floor(Math.random() * 20) + 80;
 
     if (!allowDraw && team1Score === team2Score) {
         if (team1Wins) {
@@ -21,11 +20,13 @@ function simulateGame(team1, team2, allowDraw = false) {
         }
     }
 
+    
     return {
         winner: team1Wins ? team1 : team2,
         loser: team1Wins ? team2 : team1,
         score: team1Wins ? `${team1Score}:${team2Score}` : `${team2Score}:${team1Score}`
     };
+
 }
 
 // Funkcija za Ažuriranje Statistika Tima
